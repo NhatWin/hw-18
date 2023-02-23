47b1a7cd296d36c1ab778107f6ceba88c0fa467e
NhatWin <nhat0933@gmail.com>
2023-02-22 21:32:34 -0600

    Work on models

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "A username is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "A email is required"],
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+  thoughts: [],
+  friends: [],
+});
+
+userSchema.virtual("userFriends").get(function () {
+  return this.friends.length;
+});
+
+export default module("User", userSchema);
